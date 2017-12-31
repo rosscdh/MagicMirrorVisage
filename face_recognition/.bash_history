b6d899e456ff09e81b47dd2ec751b3e485acd14b
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2017-12-31 10:55:37 +0100

    stuff

--- face_recognition/.bash_history
+++ face_recognition/.bash_history
@@ -3,3 +3,20 @@ ls
 cd examples/
 ls
 python3 cam.py 
+ls
+cd examples/
+ls
+python3 cam.py 
+pip install cv2
+pip3 install opencv-python
+python3 cam.py 
+python3 cam.py 
+pwd
+cd /root/
+ls
+cd examples/
+ls
+cat cam.py m
+cat cam.py 
+python3 cam.py 
+python3 cam.py 
--- face_recognition/examples/cam.py
+++ face_recognition/examples/cam.py
@@ -1,8 +1,8 @@
 import cv2
 import sys
-import pdb;pdb.set_trace()
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
+
+#cascPath = sys.argv[1]
+#faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
 
@@ -12,17 +12,17 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=0#cv2.CV_HAAR_SCALE_IMAGE
-    )
+    # faces = faceCascade.detectMultiScale(
+    #     gray,
+    #     scaleFactor=1.1,
+    #     minNeighbors=5,
+    #     minSize=(30, 30),
+    #     flags=0#cv2.CV_HAAR_SCALE_IMAGE
+    # )
 
     # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # for (x, y, w, h) in faces:
+    #     cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
